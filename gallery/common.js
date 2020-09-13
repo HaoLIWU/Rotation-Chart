93b4936597fb7ae9ca48b975c2dd6a3086119432
HaoLiWu <1596475749@qq.com>
2020-09-13 16:10:08 +0800

    '美女画廊'

---
+++ gallery/common.js
@@ -0,0 +1,11 @@
+function my$(id){
+    return document.getElementById(id);
+}
+//鼠标进入事件
+function mouseoverHandle(){
+    this.style.backgroundColor = "yellow";
+}
+//鼠标离开事件
+function mouseoutHandle(){
+    this.style.backgroundColor = "";
+}
